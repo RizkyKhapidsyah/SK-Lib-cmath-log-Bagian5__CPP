7f83d842a150cc413033d1717cb72d53837dfd01
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-20 11:15:59 +0700

    Add project files.

---
+++ SK-Lib-cmath-log-Bagian5__CPP/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*	Source by Programiz
+	Modified For Learn by Rizky Khapidsyah
+	I.D.E : VS2019 
+*/
+
+using namespace std;
+
+int main() {
+	int x = 2;
+	double result;
+
+	result = log(x);
+	cout << "log(x) = " << result << endl;
+
+	_getch();
+	return 0;
+}
